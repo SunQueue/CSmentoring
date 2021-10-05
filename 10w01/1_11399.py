5207712199832e302517e8af200a129aa85f490c
SunQueue <peterkim9014@gmail.com>
2021-10-05 13:21:21 +0900

    이건 왜 된거지

--- 10w01/1_11399.py
+++ 10w01/1_11399.py
@@ -1,17 +1,14 @@
 # 대충 입력받은 배열이 오름차순으로 되어있을때, 
 # n+1번째 시간 += 0~n번째 시간의 합
-
-if __name__ == "__main__":
-    # get input index
-    index = int(input())
-    # get input array
-    customer = []
-    customer = input().split()
-    # arr.sort()
-    customer.sort()
-    # for i in range (0,inputIndex)
-    #    accTime += arr[i]
-    accTime =0
-    for i in range(0,index) :
-        accTime += int(customer[i]) * (index-i)
-    print(accTime)
+# get input index
+index = int(input())
+# get input array
+#customer = []
+#customer = input().split()
+customer = list(map(int, input().split()))
+# arr.sort()
+customer.sort()
+accTime =0
+for i in range(0,index) :
+    accTime += int(customer[i]) * (index-i)
+print(accTime)
